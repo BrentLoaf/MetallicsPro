d44f12f6f4b2411d6edb1f4339ec6776f2502f30
BrentLoaf <kcoldoowtnerbg@gmail.com>
2026-03-20 23:55:16 -0700

    Removed some abstract methods from OreItem.java and overriden methods from LowGradeMetallicOreItem.java

--- src/main/java/org/brent/triggerlib/metallicspro/items/ore/LowGradeMetallicOreItem.java
+++ src/main/java/org/brent/triggerlib/metallicspro/items/ore/LowGradeMetallicOreItem.java
@@ -1,24 +1,11 @@
 package org.brent.triggerlib.metallicspro.items.ore;
 
 import org.brent.triggerlib.metallicspro.items.Rarity;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
 
 public class LowGradeMetallicOreItem extends OreItem {
 
     public LowGradeMetallicOreItem() {
         super("Low-Grade Metallic Ore", Material.RAW_COPPER, Rarity.COMMON);
-    }
-
-    @Override
-    public ItemStack getItemStack() {
-        ItemStack itemStack = getStarterStack();
-
-        itemStack.editMeta(meta -> {
-            meta.setDisplayName(itemName("Low Grade Metallic Ore", ChatColor.WHITE));
-        });
-
-        return itemStack;
     }
 }
--- src/main/java/org/brent/triggerlib/metallicspro/items/ore/OreItem.java
+++ src/main/java/org/brent/triggerlib/metallicspro/items/ore/OreItem.java
@@ -2,23 +2,11 @@ package org.brent.triggerlib.metallicspro.items.ore;
 
 import org.brent.triggerlib.metallicspro.items.CustomItem;
 import org.brent.triggerlib.metallicspro.items.Rarity;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
 
 public abstract class OreItem extends CustomItem {
 
     public OreItem(String name, Material baseMaterial, Rarity rarity) {
         super(name, baseMaterial, rarity);
-    }
-
-    public abstract ItemStack getItemStack();
-
-    public ItemStack getStarterStack() {
-        return new ItemStack(baseMaterial);
-    }
-
-    public String itemName(String name, ChatColor color) {
-        return ChatColor.RESET + "" + color + "" + name;
     }
 }
